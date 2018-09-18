b3559feb7ed676f5fe0a9e43818ca80279748ae0
user <user@031>
2018-09-18 16:09:52 +0800

    draw

--- src/main/java/lendle/courses/window_programming/componentproperties/PaintComponentSample.java
+++ src/main/java/lendle/courses/window_programming/componentproperties/PaintComponentSample.java
@@ -19,6 +19,17 @@ import javax.swing.WindowConstants;
 public class PaintComponentSample extends JPanel{
     //利用 paintComponent 讓 JPanel 畫出一個橢圓
     
+    @Override
+    protected void paintComponent(Graphics g) {
+        int width = this.getWidth();
+        int height = this.getHeight();
+        g.setColor(Color.red);
+        g.drawOval(0, 0, width, height);
+        g.setColor(Color.blue);
+        g.drawLine(0, 0, width, height);
+        g.draw3DRect(0, 0, width, height, false);
+    }
+
     //////////////////////////////////////////
     /**
      * @param args the command line arguments
